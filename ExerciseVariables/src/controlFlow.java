9c58eac5cead1f89f603360439723a794baf6d6b
Corey Barr <corey.jbarr@gmail.com>
2021-02-26 11:10:04 -0600

    scanner questions done

--- ExerciseVariables/src/controlFlow.java
+++ ExerciseVariables/src/controlFlow.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class controlFlow {
     
-    //Question 1
+    public static void main(String[] args) {
+        prog1();
+        prog2();
+        prog3();
+        prog4();
+        prog5();
+        prog6();
+    
+    }
+        //Question 1
     public static void prog1(){
        int x = 7;
        if(x<10){
@@ -41,11 +52,81 @@ public class controlFlow {
             System.out.println("In range");
         }
     }
-    public static void main(String[] args) {
-        prog1();
-        prog2();
-        prog3();
-        prog4();
+
+    //Question 5
+    public static void prog5(){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter score:");
+        int x = input.nextInt();
+        String grade = "";
+        if(x>=90 && x<=100){
+            grade = "A";
+            System.out.println("Grade: " + grade);
+        }
+        else if (x >= 80 && x < 90) {
+            grade = "B";
+            System.out.println("Grade: " + grade);
+        }
+         else if(x>=70 && x<80){
+            grade = "C";
+            System.out.println("Grade: " + grade);
+        }
+       else if (x >= 60 && x < 70) {
+            grade = "D";
+            System.out.println("Grade: " + grade);
+        }
+        else if (x < 60) {
+            grade = "F";
+            System.out.println("Grade: "+ grade);
+        }
+        else{
+            System.out.println("Score out of Range");
+        }
+  
     }
 
+    //Question 6
+    public static void prog6(){
+        String weekDay="";
+        Scanner dayScan = new Scanner(System.in);
+        System.out.println("Enter number 1-7:");
+        int x = dayScan.nextInt();
+        if(x<1||x>7){
+            System.out.println("Reenter a correct number");
+            prog6();
+        }
+        else if(x>=1 && x<=7 ){
+            switch (x){
+                case 1: weekDay="Sunday";
+                break;
+                case 2:
+                weekDay = "Monday";
+                break;
+                case 3:
+                weekDay = "Tuesday";
+                break;
+                 case 4: weekDay="Wednesday";
+                break;
+
+                 case 5: weekDay="Thursday";
+                break;
+
+                 case 6: weekDay="Friday";
+                break;
+
+                case 7:
+                weekDay = "Saturday";
+                break;
+
+
+
+            }
+        }
+
+        System.out.println(weekDay);
+        dayScan.close();
+    }
 }
+
+
